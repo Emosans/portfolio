b1fc17a221677cf11cf7d6ae17b5d1d3b87005d5
Emosans <shreyasd1414@gmail.com>
2024-12-24 23:40:22 +0530

    :truck: changes

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 app = Flask(__name__)
 
-@app.route("/index.html")
+@app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/works.html")
-def works():
-    return render_template('works.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route("/contact.html")
-def contact():
-    return render_template('contact.html')
+def write_to_file(data):
+    email=data["email"]
+    subject=data["subject"]
+    message=data["message"]
+    with open('contact.txt',mode='a') as database:
+        file=database.write(f'\n{email},{subject},{message}')
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
-
-@app.route("/work.html")
-def work():
-    return render_template("work.html")
+@app.route("/submit_form",methods=["POST","GET"])
+def submit_form():
+    if request.method=='POST':
+        data=request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'

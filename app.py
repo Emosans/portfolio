bce31f534ad7a9214e891a6cdfa535cb1c4139c2
Emosans <shreyasd1414@gmail.com>
2024-12-24 16:47:20 +0530

    :truck: templates

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world() -> str:
-    return 'Hello World!'
+@app.route("/index.html")
+def home():
+    return render_template('index.html')
+
+@app.route("/works.html")
+def works():
+    return render_template('works.html')
+
+@app.route("/contact.html")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/work.html")
+def work():
+    return render_template("work.html")
